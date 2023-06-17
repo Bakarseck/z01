d59187920a2338e8b4074de6a6179e324b283d8f
bakseck <seck.bakar@ugb.edu.sn>
2023-06-17 23:01:48 +0000

    initializingFrameword

---
+++ utils.go
@@ -0,0 +1,27 @@
+package z01
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+)
+
+func LoadEnv(path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Println("🚨 " + err.Error())
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) != 2 {
+			log.Println("🚨 Your env file must be set")
+		}
+		key := parts[0]
+		value := parts[1]
+		os.Setenv(key, value)
+	}
+	return scanner.Err()
+}
